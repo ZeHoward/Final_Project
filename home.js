1371eaaab98ba9dea44376830f94c7088411cbc1
code8la888 <kazuhaha1997@gmail.com>
2024-09-13 21:15:04 +0800

    09130914

--- home.js
+++ home.js
@@ -100,46 +100,76 @@ window.onload = function () {
     },
     {
       id: 2,
-      name: "義大利餃",
-      price: 120,
-      image: "./material/mealkit/dumpling.jpg",
-    },
-    {
-      id: 3,
       name: "南洋叻沙海鮮湯麵",
       price: 135,
       image: "./material/mealkit/laksa.jpg",
     },
     {
-      id: 4,
+      id: 3,
       name: "奶油松露百菇燉飯",
       price: 220,
-      image: "./material/mealkit/奶油松露百菇燉飯.jpg",
+      image: "./material/mealkit/CreamyRisotto.jpg",
     },
     {
-      id: 5,
+      id: 4,
       name: "泰式冬蔭功酸辣海鮮湯",
       price: 170,
       image: "./material/mealkit/soup.png",
     },
     {
-      id: 6,
+      id: 5,
       name: "印度雞肉香飯",
       price: 140,
-      image: "./material/mealkit/印度雞肉香飯.jpg",
+      image: "./material/mealkit/ChickenBiryani.jpg",
+    },
+    {
+      id: 6,
+      name: "新加坡海南雞飯",
+      price: 150,
+      image: "./material/mealkit/HainaneseChickenRice.jpg",
     },
     {
       id: 7,
-      name: "新加坡海南雞飯",
+      name: "韓式五色石鍋拌飯",
+      price: 140,
+      image: "./material/mealkit/KoreanBibimbap.png",
+    },
+    {
+      id: 8,
+      name: "馬來西亞椰漿飯",
+      price: 190,
+      image: "./material/mealkit/NasiLemak.jpg",
+    },
+    {
+      id: 9,
+      name: "馬來西亞椰漿飯",
       price: 150,
-      image: "./material/mealkit/新加坡海南雞飯.jpg",
+      image: "./material/mealkit/thaiPork.jpg",
+    },
+    {
+      id: 10,
+      name: "越南牛肉河粉",
+      price: 130,
+      image: "./material/mealkit/BeefPho.png",
+    },
+    {
+      id: 11,
+      name: "希臘酥炸櫛瓜薯泥球",
+      price: 160,
+      image: "./material/mealkit/PotatoBalls.jpg",
+    },
+    {
+      id: 12,
+      name: "西班牙海鮮燉飯",
+      price: 220,
+      image: "./material/mealkit/SeafoodPaella.jpg",
     },
   ];
 
   let currentIndex = 0;
 
   function renderProducts() {
-    const container = document.getElementById("productContainer");
+    const container = document.getElementById("productContainer1");
     container.innerHTML = "";
     for (let i = -1; i <= 1; i++) {
       const index = (currentIndex + i + products.length) % products.length;
@@ -147,13 +177,13 @@ window.onload = function () {
       const productElement = document.createElement("div");
       productElement.className = "product";
       productElement.innerHTML = `
-                <img src="${product.image}" alt="${product.name}">
-                <h4>${product.name}</h4>
-                <p>$${product.price}</p>
-                <div class="home-product-btn">
-                  <button class="add-to-favorite"><i class="fa-solid fa-heart"></i></button>
-                  <button class="add-to-cart"><i class="fa-solid fa-cart-shopping"></i>&nbsp;&nbsp;&nbsp;加入購物車</button>
-                </div>
+                <img class="product-image" src="${product.image}" alt="${product.name}">
+                <h3 class="product-name">${product.name}</h3>
+                <p class="product-price">$NT${product.price}</p>
+                  <div class="home-product-btn">
+                    <button class="add-to-favorite"><i class="fa-solid fa-heart"></i></button>
+                    <button class="add-to-cart"><i class="fa-solid fa-cart-shopping"></i>&nbsp;&nbsp;&nbsp;加入購物車</button>
+                  </div>
             `;
       container.appendChild(productElement);
     }
@@ -162,7 +192,7 @@ window.onload = function () {
     document.querySelectorAll(".add-to-cart").forEach((button) => {
       button.addEventListener("click", function () {
         const productName =
-          this.closest(".product").querySelector("h4").textContent;
+          this.closest(".product").querySelector("h3").textContent;
         alert(`已將 ${productName} 加入購物車！`);
       });
     });
@@ -171,13 +201,29 @@ window.onload = function () {
     document.querySelectorAll(".add-to-favorite").forEach((button) => {
       button.addEventListener("click", function () {
         const productName =
-          this.closest(".product").querySelector("h4").textContent;
-        alert(`已將 ${productName} 加入收藏！`);
-        this.style.color = "#cc4235";
+          this.closest(".product").querySelector("h3").textContent;
+
+        // 切換收藏狀態
+        if (this.classList.contains("favorited")) {
+          this.classList.remove("favorited");
+
+          // 先更改顏色，然後延遲彈跳框
+          setTimeout(() => {
+            alert(`已將 ${productName} 取消收藏！`);
+          }, 500); // 300 毫秒延遲
+        } else {
+          this.classList.add("favorited");
+
+          // 先更改顏色，然後延遲彈跳框
+          setTimeout(() => {
+            alert(`已將 ${productName} 加入收藏！`);
+          }, 500); // 300 毫秒延遲
+        }
       });
     });
   }
 
+  //上下一個商品
   document.getElementById("prevBtn").addEventListener("click", () => {
     currentIndex = (currentIndex - 1 + products.length) % products.length;
     renderProducts();
@@ -200,39 +246,69 @@ window.onload = function () {
     },
     {
       id: 2,
-      name: "義大利餃",
-      price: 120,
-      image: "./material/mealkit/dumpling.jpg",
-    },
-    {
-      id: 3,
       name: "南洋叻沙海鮮湯麵",
       price: 135,
       image: "./material/mealkit/laksa.jpg",
     },
     {
-      id: 4,
+      id: 3,
       name: "奶油松露百菇燉飯",
       price: 220,
-      image: "./material/mealkit/奶油松露百菇燉飯.jpg",
+      image: "./material/mealkit/CreamyRisotto.jpg",
     },
     {
-      id: 5,
+      id: 4,
       name: "泰式冬蔭功酸辣海鮮湯",
       price: 170,
       image: "./material/mealkit/soup.png",
     },
     {
-      id: 6,
+      id: 5,
       name: "印度雞肉香飯",
       price: 140,
-      image: "./material/mealkit/印度雞肉香飯.jpg",
+      image: "./material/mealkit/ChickenBiryani.jpg",
+    },
+    {
+      id: 6,
+      name: "新加坡海南雞飯",
+      price: 150,
+      image: "./material/mealkit/HainaneseChickenRice.jpg",
     },
     {
       id: 7,
-      name: "新加坡海南雞飯",
+      name: "韓式五色石鍋拌飯",
+      price: 140,
+      image: "./material/mealkit/KoreanBibimbap.png",
+    },
+    {
+      id: 8,
+      name: "馬來西亞椰漿飯",
+      price: 190,
+      image: "./material/mealkit/NasiLemak.jpg",
+    },
+    {
+      id: 9,
+      name: "馬來西亞椰漿飯",
       price: 150,
-      image: "./material/mealkit/新加坡海南雞飯.jpg",
+      image: "./material/mealkit/thaiPork.jpg",
+    },
+    {
+      id: 10,
+      name: "越南牛肉河粉",
+      price: 130,
+      image: "./material/mealkit/BeefPho.png",
+    },
+    {
+      id: 11,
+      name: "希臘酥炸櫛瓜薯泥球",
+      price: 160,
+      image: "./material/mealkit/PotatoBalls.jpg",
+    },
+    {
+      id: 12,
+      name: "西班牙海鮮燉飯",
+      price: 220,
+      image: "./material/mealkit/SeafoodPaella.jpg",
     },
   ];
 
@@ -249,15 +325,15 @@ window.onload = function () {
       const productElement2 = document.createElement("div");
       productElement2.className = "product";
       productElement2.innerHTML = `
-                <img src="${product2.image}" alt="${product2.name}">
-                <h4>${product2.name}</h4>
-                <p>$${product2.price}</p>
-                <div class="home-product-btn">
-                  <button class="add-to-favorite"><i class="fa-solid fa-heart"></i></button>
-                  <button class="add-to-cart"><i class="fa-solid fa-cart-shopping"></i>&nbsp;&nbsp;&nbsp;加入購物車</button>
-                </div>
+                <img class="product-image" src="${product2.image}" alt="${product2.name}">
+                <h3 class="product-name">${product2.name}</h3>
+                <p class="product-price">$NT${product2.price}</p>
+                  <div class="home-product-btn">
+                    <button class="add-to-favorite"><i class="fa-solid fa-heart"></i></button>
+                    <button class="add-to-cart"><i class="fa-solid fa-cart-shopping"></i>&nbsp;&nbsp;&nbsp;加入購物車</button>
+                  </div>  
             `;
-      container2.appendChild(productElement2); // 使用 productElement2 而不是 productElement
+      container2.appendChild(productElement2);
     }
 
     // 加入購物車按鈕事件處理
@@ -269,15 +345,30 @@ window.onload = function () {
       });
     });
 
-    // 收藏商品按鈕事件處理
-    document.querySelectorAll(".add-to-favorite").forEach((button) => {
-      button.addEventListener("click", function () {
-        const productName2 =
-          this.closest(".product").querySelector("h4").textContent;
-        alert(`已將 ${productName2} 加入收藏！`);
-        this.style.color = "#cc4235";
-      });
-    });
+    // // 收藏商品按鈕事件處理
+    // document.querySelectorAll(".add-to-favorite").forEach((button) => {
+    //   button.addEventListener("click", function () {
+    //     const productName =
+    //       this.closest(".product").querySelector("h3").textContent;
+
+    //     // 切換收藏狀態
+    //     if (this.classList.contains("favorited")) {
+    //       this.classList.remove("favorited");
+
+    //       // 先更改顏色，然後延遲彈跳框
+    //       setTimeout(() => {
+    //         alert(`已將 ${productName} 取消收藏！`);
+    //       }, 500); // 300 毫秒延遲
+    //     } else {
+    //       this.classList.add("favorited");
+
+    //       // 先更改顏色，然後延遲彈跳框
+    //       setTimeout(() => {
+    //         alert(`已將 ${productName} 加入收藏！`);
+    //       }, 500); // 300 毫秒延遲
+    //     }
+    //   });
+    // });
   }
 
   document.getElementById("prevBtn2").addEventListener("click", () => {
--- product.js
+++ product.js
@@ -50,7 +50,6 @@ window.onload = function () {
 
   var dropdown2 = document.getElementsByClassName("down-btn");
   var x;
-
   for (x = 0; x < dropdown2.length; x++) {
     dropdown2[x].addEventListener("click", function () {
       this.classList.toggle("active2");
@@ -63,18 +62,36 @@ window.onload = function () {
     });
   }
 
+  //menu toggle up/down 圖案
+  window.togglePic3 = function () {
+    // var margin1 = document.getElementById("add");
+    var img3 = document.getElementById("updown3");
+    if (img3.src.includes("down.png")) {
+      img3.src = "./material/icon/up.png";
+      // margin1.style.margin = "40px 0";
+    } else {
+      img3.src = "./material/icon/down.png";
+      // margin1.style.margin = "";
+    }
+  };
+  window.togglePic4 = function () {
+    // var margin2 = document.getElementById("add");
+    var img4 = document.getElementById("updown4");
+    if (img4.src.includes("down.png")) {
+      img4.src = "./material/icon/up.png";
+      // margin2.style.margin = "40px 0";
+    } else {
+      img4.src = "./material/icon/down.png";
+      // margin2.style.margin = "";
+    }
+  };
+
   const products = [
     {
       name: "日式豬排飯",
       price: 130,
       image: "./material/mealkit/katsudon.jpg",
       releaseDate: "2023-04-22",
-    },
-    {
-      name: "義大利餃",
-      price: 120,
-      image: "./material/mealkit/dumpling.jpg",
-      releaseDate: "2021-03-22",
     },
     {
       name: "南洋叻沙海鮮湯麵",
@@ -85,7 +102,7 @@ window.onload = function () {
     {
       name: "奶油松露百菇燉飯",
       price: 220,
-      image: "./material/mealkit/奶油松露百菇燉飯.jpg",
+      image: "./material/mealkit/CreamyRisotto.jpg",
       releaseDate: "2025-06-22",
     },
     {
@@ -97,38 +114,62 @@ window.onload = function () {
     {
       name: "印度雞肉香飯",
       price: 140,
-      image: "./material/mealkit/印度雞肉香飯.jpg",
+      image: "./material/mealkit/ChickenBiryani.jpg",
       releaseDate: "2023-02-22",
     },
     {
       name: "新加坡海南雞飯",
       price: 150,
-      image: "./material/mealkit/新加坡海南雞飯.jpg",
+      image: "./material/mealkit/HainaneseChickenRice.jpg",
       releaseDate: "2021-07-22",
     },
     {
-      name: "新加坡海南雞飯",
+      name: "韓式五色石鍋拌飯",
       price: 140,
-      image: "./material/mealkit/新加坡海南雞飯.jpg",
+      image: "./material/mealkit/KoreanBibimbap.png",
       releaseDate: "2021-11-22",
     },
     {
-      name: "新加坡海南雞飯",
+      name: "馬來西亞椰漿飯",
       price: 190,
-      image: "./material/mealkit/新加坡海南雞飯.jpg",
+      image: "./material/mealkit/NasiLemak.jpg",
+      releaseDate: "2021-04-01",
+    },
+    {
+      name: "馬來西亞椰漿飯",
+      price: 150,
+      image: "./material/mealkit/thaiPork.jpg",
+      releaseDate: "2021-04-01",
+    },
+    {
+      name: "越南牛肉河粉",
+      price: 130,
+      image: "./material/mealkit/BeefPho.png",
+      releaseDate: "2021-04-01",
+    },
+    {
+      name: "希臘酥炸櫛瓜薯泥球",
+      price: 160,
+      image: "./material/mealkit/PotatoBalls.jpg",
+      releaseDate: "2021-04-01",
+    },
+    {
+      name: "西班牙海鮮燉飯",
+      price: 220,
+      image: "./material/mealkit/SeafoodPaella.jpg",
       releaseDate: "2021-04-01",
     },
   ];
 
   window.displayProducts = function (products) {
-    const container = document.getElementById("pcontainer");
+    const container = document.getElementById("productContainer");
     container.innerHTML = ""; // Clear existing products
     products.forEach((product) => {
       container.innerHTML += `
-              <div class="product-card">
+              <div class="product">
                   <img class="product-image" src="${product.image}" alt="${product.name}">
-                  <h3>${product.name}</h3>
-                  <p class="product-price">$${product.price}</p>
+                  <h3 class="product-name">${product.name}</h3>
+                  <p class="product-price">$NT${product.price}</p>
                   <div class="home-product-btn">
                     <button class="add-to-favorite"><i class="fa-solid fa-heart"></i></button>
                     <button class="add-to-cart"><i class="fa-solid fa-cart-shopping"></i>&nbsp;&nbsp;&nbsp;加入購物車</button>
@@ -141,7 +182,7 @@ window.onload = function () {
     document.querySelectorAll(".add-to-cart").forEach((button) => {
       button.addEventListener("click", function () {
         const productName =
-          this.closest(".product-card").querySelector("h3").textContent;
+          this.closest(".product").querySelector("h3").textContent;
         alert(`已將 ${productName} 加入購物車！`);
       });
     });
@@ -150,9 +191,24 @@ window.onload = function () {
     document.querySelectorAll(".add-to-favorite").forEach((button) => {
       button.addEventListener("click", function () {
         const productName =
-          this.closest(".product-card").querySelector("h3").textContent;
-        alert(`已將 ${productName} 加入收藏！`);
-        this.style.color = "#cc4235";
+          this.closest(".product").querySelector("h3").textContent;
+
+        // 切換收藏狀態
+        if (this.classList.contains("favorited")) {
+          this.classList.remove("favorited");
+
+          // 先更改顏色，然後延遲彈跳框
+          setTimeout(() => {
+            alert(`已將 ${productName} 取消收藏！`);
+          }, 500); // 300 毫秒延遲
+        } else {
+          this.classList.add("favorited");
+
+          // 先更改顏色，然後延遲彈跳框
+          setTimeout(() => {
+            alert(`已將 ${productName} 加入收藏！`);
+          }, 500); // 300 毫秒延遲
+        }
       });
     });
   };
@@ -172,6 +228,12 @@ window.onload = function () {
         (a, b) => new Date(a.releaseDate) - new Date(b.releaseDate)
       );
     }
+    displayProducts(products);
+  };
+
+  // 預設排序為隨機排序
+  window.onload = function () {
+    products.sort(() => Math.random() - 0.5); // 隨機排序
     displayProducts(products);
   };
 
