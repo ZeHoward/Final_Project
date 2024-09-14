aeb97832db67ecf4161325617690d7e36b0db5a4
Jing-Wen-Hsu <wen23479@gmail.com>
2024-09-14 18:45:14 +0800

    結帳填寫資訊頁完成套版

--- checkout.js
+++ checkout.js
@@ -1,3 +1,56 @@
+
+window.onload = function () {
+  //菜單展開、關閉
+  window.openSidenav = function () {
+    document.getElementById("sidenav").style.width = "100%";
+    document.body.style.overflow = "hidden";
+  };
+
+  window.closeSidenav = function () {
+    document.getElementById("sidenav").style.width = "0%";
+    document.body.style.overflow = "";
+  };
+
+  //展開菜單選項
+  var dropdowns = document.getElementsByClassName("tem-dropdown-btn");
+  for (var i = 0; i < dropdowns.length; i++) {
+    dropdowns[i].addEventListener("click", function () {
+      this.classList.toggle("tem-active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      } else {
+        dropdownContent.style.display = "block";
+      }
+    });
+  }
+
+  //menu toggle up/down 圖案
+  window.togglePic1 = function () {
+    var margin1 = document.getElementById("add");
+    var img1 = document.getElementById("updown1");
+    if (img1.src.includes("down.png")) {
+      img1.src = "./material/icon/up.png";
+      margin1.style.margin = "40px 0";
+    } else {
+      img1.src = "./material/icon/down.png";
+      margin1.style.margin = "";
+    }
+  };
+  window.togglePic2 = function () {
+    var margin2 = document.getElementById("add");
+    var img2 = document.getElementById("updown2");
+    if (img2.src.includes("down.png")) {
+      img2.src = "./material/icon/up.png";
+      margin2.style.margin = "40px 0";
+    } else {
+      img2.src = "./material/icon/down.png";
+      margin2.style.margin = "";
+    }
+  };
+};
+
+
 // 縣市區域及郵遞區號
 const districts = {
   taipei: {
