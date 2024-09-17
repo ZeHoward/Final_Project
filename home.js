f094c2ff1ddb5f70f63b634f0af63d220655f5c6
code8la888 <kazuhaha1997@gmail.com>
2024-09-17 14:12:45 +0800

    增加商品卡模板

--- home.js
+++ home.js
@@ -188,7 +188,7 @@ window.onload = function () {
   function renderProducts() {
     const container = document.getElementById("productContainer1");
     container.innerHTML = "";
-    for (let i = -1; i <= 1; i++) {
+    for (let i = -1; i <= 2; i++) {
       const index = (currentIndex + i + products.length) % products.length;
       const product = products[index];
       const productElement = document.createElement("div");
@@ -334,7 +334,7 @@ window.onload = function () {
   function renderProducts2() {
     const container2 = document.getElementById("productContainer2");
     container2.innerHTML = "";
-    for (let i = -1; i <= 1; i++) {
+    for (let i = -1; i <= 2; i++) {
       const index =
         (currentIndex2 + i + meal_kit_products.length) %
         meal_kit_products.length;
@@ -357,35 +357,35 @@ window.onload = function () {
     document.querySelectorAll(".add-to-cart").forEach((button) => {
       button.addEventListener("click", function () {
         const productName2 =
-          this.closest(".product").querySelector("h4").textContent;
+          this.closest(".product").querySelector("h3").textContent;
         alert(`已將 ${productName2} 加入購物車！`);
       });
     });
 
-    // // 收藏商品按鈕事件處理
-    // document.querySelectorAll(".add-to-favorite").forEach((button) => {
-    //   button.addEventListener("click", function () {
-    //     const productName =
-    //       this.closest(".product").querySelector("h3").textContent;
+    // 收藏商品按鈕事件處理
+    document.querySelectorAll(".add-to-favorite").forEach((button) => {
+      button.addEventListener("click", function () {
+        const productName =
+          this.closest(".product").querySelector("h3").textContent;
 
-    //     // 切換收藏狀態
-    //     if (this.classList.contains("favorited")) {
-    //       this.classList.remove("favorited");
+        // 切換收藏狀態
+        if (this.classList.contains("favorited")) {
+          this.classList.remove("favorited");
 
-    //       // 先更改顏色，然後延遲彈跳框
-    //       setTimeout(() => {
-    //         alert(`已將 ${productName} 取消收藏！`);
-    //       }, 500); // 300 毫秒延遲
-    //     } else {
-    //       this.classList.add("favorited");
+          // 先更改顏色，然後延遲彈跳框
+          setTimeout(() => {
+            alert(`已將 ${productName} 取消收藏！`);
+          }, 500); // 300 毫秒延遲
+        } else {
+          this.classList.add("favorited");
 
-    //       // 先更改顏色，然後延遲彈跳框
-    //       setTimeout(() => {
-    //         alert(`已將 ${productName} 加入收藏！`);
-    //       }, 500); // 300 毫秒延遲
-    //     }
-    //   });
-    // });
+          // 先更改顏色，然後延遲彈跳框
+          setTimeout(() => {
+            alert(`已將 ${productName} 加入收藏！`);
+          }, 500); // 300 毫秒延遲
+        }
+      });
+    });
   }
 
   document.getElementById("prevBtn2").addEventListener("click", () => {
