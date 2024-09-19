28822efbb7d26d9b9d6cbb769953aca90f9df55a
ZeHoward <howard867@yahoo.com.tw>
2024-09-19 21:52:51 +0800

    09190952 乾淨測試spring

---
+++ final/src/test/java/tw/howard/final_project/FinalApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.howard.final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
