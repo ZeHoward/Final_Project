0ecc1888a141b87c4420c1190c2bdb96f272f8e8
ZeHoward <howard867@yahoo.com.tw>
2024-09-23 21:17:20 +0800

    09232117

--- Final_Project/FinalTest/src/main/java/tw/luna/FinalTest/repository/ProductImageRepository.java
+++
@@ -1,9 +0,0 @@
-package tw.luna.FinalTest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import tw.luna.FinalTest.model.ProductImage;
-
-public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
-	
-}
