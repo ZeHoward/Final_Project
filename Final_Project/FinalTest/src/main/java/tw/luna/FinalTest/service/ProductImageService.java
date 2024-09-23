85e1a7b8323245d9de1cc50233f8909f6802a5fa
code8la888 <kazuhaha1997@gmail.com>
2024-09-23 13:59:13 +0800

    0923

---
+++ Final_Project/FinalTest/src/main/java/tw/luna/FinalTest/service/ProductImageService.java
@@ -0,0 +1,16 @@
+package tw.luna.FinalTest.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import tw.luna.FinalTest.model.ProductImage;
+
+public interface ProductImageService {
+	List<ProductImage> findAllImages();
+	
+	Optional<ProductImage> findProductImageById(Long id);
+	
+	ProductImage saveProductImage(ProductImage productImage);
+	
+	void deleteProductImage(Long id);
+}
