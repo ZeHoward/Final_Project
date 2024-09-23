e693b48accc04c68a29ddece115a27e72b0e8458
ZeHoward <howard867@yahoo.com.tw>
2024-09-23 17:08:10 +0800

    09231708

--- final/src/main/java/tw/final_project/FinalApplication.java
+++ final/src/main/java/tw/final_project/FinalApplication.java
@@ -2,14 +2,16 @@ package tw.final_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FinalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinalApplication.class, args);
 	}
- 
+
 }
 
 // 這個檔案不准動，動了會爆炸
