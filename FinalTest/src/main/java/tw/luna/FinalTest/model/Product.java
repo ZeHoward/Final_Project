181532babb9b237f063dd12a3cfe66c12fc7736f
code8la888 <kazuhaha1997@gmail.com>
2024-09-27 00:03:56 +0800

    09271203 add base64

--- FinalTest/src/main/java/tw/luna/FinalTest/model/Product.java
+++ FinalTest/src/main/java/tw/luna/FinalTest/model/Product.java
@@ -10,6 +10,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 @Table(name="products")
