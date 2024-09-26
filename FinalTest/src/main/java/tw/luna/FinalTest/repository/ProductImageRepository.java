d68a7013b7613640ff31063d286f47bd7cca01f0
lunawewe <ls22012900@gmail.com>
2024-09-26 15:44:05 +0800

    新增使用者收藏商品的Repo, Con, Mod, Ser

--- FinalTest/src/main/java/tw/luna/FinalTest/repository/ProductImageRepository.java
+++ FinalTest/src/main/java/tw/luna/FinalTest/repository/ProductImageRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import tw.luna.FinalTest.model.ProductImage;
 
+import java.util.List;
+
 public interface ProductImageRepository extends JpaRepository<ProductImage, Long> {
-	
+    // 查找與某個商品 ID 相關的所有圖片
+    List<ProductImage> findByProductProductId(Integer productId);
 }
