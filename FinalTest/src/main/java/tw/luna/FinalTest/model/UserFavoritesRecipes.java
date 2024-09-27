7b4ebce34d39599a58422f6084ea1976dc867dcc
lunawewe <Ls22012900#gmail.com>
2024-09-27 09:17:30 +0800

    新增使用者收藏食譜的Repo, Con, Mod, Ser

--- FinalTest/src/main/java/tw/luna/FinalTest/model/UserFavoritesRecipes.java
+++ FinalTest/src/main/java/tw/luna/FinalTest/model/UserFavoritesRecipes.java
@@ -1,5 +1,5 @@
-//package tw.luna.FinalTest.model;
-//
+package tw.luna.FinalTest.model;
+
 //import com.fasterxml.jackson.annotation.JsonBackReference;
 //
 //import jakarta.persistence.EmbeddedId;
@@ -22,3 +22,43 @@
 //	@MapsId("userId")
 //	private Users users;
 //}
+
+import jakarta.persistence.*;
+import tw.luna.FinalTest.model.UserFavoritesRecipesId;
+
+@Entity
+@Table(name = "userfavoritesrecipes")
+public class UserFavoritesRecipes {
+
+    @EmbeddedId
+    private UserFavoritesRecipesId id;
+
+    // Constructors
+    public UserFavoritesRecipes() {}
+
+    public UserFavoritesRecipes(UserFavoritesRecipesId id) {
+        this.id = id;
+    }
+
+    public UserFavoritesRecipes(Long userId, int recipeId) {
+        this.id = new UserFavoritesRecipesId(userId, recipeId);
+    }
+
+    // Getters and Setters
+    public UserFavoritesRecipesId getId() {
+        return id;
+    }
+
+    public void setId(UserFavoritesRecipesId id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return id.getUserId();
+    }
+
+    public int getRecipeId() {
+        return id.getRecipeId();
+    }
+}
+
