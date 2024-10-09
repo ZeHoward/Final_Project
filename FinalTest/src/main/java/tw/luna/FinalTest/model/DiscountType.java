ca347c3ffa8615ddc2343d8edc8d2070cdc665a0
ZeHoward <howard867@yahoo.com.tw>
2024-10-09 14:28:12 +0800

    Delete FinalTest directory

--- FinalTest/src/main/java/tw/luna/FinalTest/model/DiscountType.java
+++
@@ -1,7 +0,0 @@
-package tw.luna.FinalTest.model;
-
-public enum DiscountType {
-    percentage, // 小寫與資料庫中的值一致
-    amount;
-}
-
--- FinalTest/src/main/java/tw/luna/FinalTest/model/UsersStatus.java
+++
@@ -1,6 +0,0 @@
-package tw.luna.FinalTest.model;
-
-public enum UsersStatus {
-	OK, EXIST, NOT_EXIST, ADD_SUCCESS, ADD_FAILURE,
-	LOGIN_SUCCESS, LOGIN_FAILURE;
-}
