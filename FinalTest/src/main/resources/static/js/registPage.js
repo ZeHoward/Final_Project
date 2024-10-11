6871cc24ff5e4ebb49e8a78b596b2e91e651ad76
PANJAY <jay66347@gmail.com>
2024-10-11 11:27:02 +0800

    修正註冊無法正確驗證密碼問題

--- FinalTest/src/main/resources/static/js/registPage.js
+++ FinalTest/src/main/resources/static/js/registPage.js
@@ -2,6 +2,7 @@ window.onload = function () {
 	let registPassword = false;
     let checkPassword = false;
     let finallyPassword = '';
+	let finallyPassword2 = '';
     let finallyEmail = '';
 
     document.getElementById('registEmailInput').addEventListener('blur', async (event) => {
@@ -15,38 +16,35 @@ window.onload = function () {
         emailRegexCheck.style.opacity = 0;
       }
     });
+	
+	
+	document.getElementById('registPasswordInput').addEventListener('input',async () => {
+		  finallyPassword = '';
+		  let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;      
+		  let password = document.getElementById('registPasswordInput').value;
+	      if(!regex.test(password)){
+	        document.getElementById('passwordRegexCheck').style.opacity = 1;
+	      }else{
+	        document.getElementById('passwordRegexCheck').style.opacity = 0;
+			finallyPassword = password;
+	      }
+
+	    });
 
 
-    document.getElementById('registPasswordCheckInput').addEventListener('blur', async () => {
-      let password01 = document.getElementById('registPasswordInput').value;
+    document.getElementById('registPasswordCheckInput').addEventListener('input', async () => {
+	  finallyPassword2 = '';
+      //let password01 = document.getElementById('registPasswordInput').value;
       let password02 = document.getElementById('registPasswordCheckInput').value;
       let passwordDifferent = document.getElementById('passwordDifferent').style;
-      if(password01 !== password02){
-        registPassword = false;
-        finallyPassword = '';
+      if(finallyPassword !== password02){
+        //finallyPassword = '';
         passwordDifferent.opacity = 1;
       }else{
-        registPassword = true;
-        finallyPassword = password02;
+        finallyPassword2 = password02;
         passwordDifferent.opacity = 0;
       }
       
-    });
-
-    
-
-    document.getElementById('registPasswordInput').addEventListener('blur',async () => {
-	  let regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/;      
-	  let password = document.getElementById('registPasswordInput').value;
-      if(!regex.test(password)){
-        document.getElementById('passwordRegexCheck').style.opacity = 1;
-        checkPassword = false;
-        finallyPassword = '';
-      }else{
-        document.getElementById('passwordRegexCheck').style.opacity = 0;
-        checkPassword = true;
-        finallyPassword = password;
-      }
     });
 
 	
@@ -54,9 +52,9 @@ window.onload = function () {
     document.getElementById('regist').addEventListener('click', () => {
       let userName = document.getElementById('registUserName').value;
 	  let birthday = document.getElementById('registBirthday').value || '';
-	  
-/*
-      if(finallyEmail != '' && finallyPassword != '' && userName != '' && birthday != ''){
+	  //alert('username:' + userName + 'finalEmail:' + finallyEmail + 'finallyPassword2' + finallyPassword2)	  
+
+      if(finallyEmail != '' && finallyPassword2 != '' && userName != '' && birthday != ''){
 		Swal.fire({
 			  title: '正在發送中...',
 			  text: '請稍後',
@@ -134,7 +132,7 @@ window.onload = function () {
 			text:"請再檢查電子信箱、密碼、使用者名稱、生日是否輸入有誤!!",
 			icon:"warning"
 		})
-      }*/
+      }
       
     });
 	
