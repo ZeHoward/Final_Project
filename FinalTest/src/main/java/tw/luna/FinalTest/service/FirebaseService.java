c9318b6141e342dd643045896a6caabec391e475
lunawewe <Ls22012900@gmail.com>
2024-10-15 11:01:41 +0800

    加密firebase敏感性資訊，修復google登入

--- FinalTest/src/main/java/tw/luna/FinalTest/service/FirebaseService.java
+++ FinalTest/src/main/java/tw/luna/FinalTest/service/FirebaseService.java
@@ -6,24 +6,36 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
-
+import java.io.InputStream;
 @Service
 public class FirebaseService {
 
     @PostConstruct
     public void initialize() {
         try {
-            FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/ee85enjoyum-firebase-adminsdk-879hb-b508264fb5.json");
+            String key = System.getenv("ENCRYPTION_KEY");
+            if (key == null) {
+                throw new IllegalStateException("Encryption key is not set in environment variables.");
+            }
+
+            ProcessBuilder processBuilder = new ProcessBuilder(
+                    "openssl", "enc", "-aes-256-cbc", "-d", "-in", "src/main/resources/ee85enjoyum-firebase-adminsdk-879hb-b508264fb5.json.enc",
+                    "-pass", "pass:" + key
+            );
+
+            Process process = processBuilder.start();
+            InputStream serviceAccount = process.getInputStream();
 
             FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
 
             FirebaseApp.initializeApp(options);
+            System.out.println("FirebaseApp has been successfully initialized.");
+
         } catch (Exception e) {
             e.printStackTrace();
+            throw new RuntimeException("Failed to initialize FirebaseApp: " + e.getMessage());
         }
     }
 }
-
